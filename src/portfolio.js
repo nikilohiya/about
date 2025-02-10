ffc29687de1b17fa246d94d9967f654266dedd03
Nikhil Lohiya <nikilohiya@gmail.com>
2025-02-09 18:17:36 -0800

    Updated the Product Role

--- src/portfolio.js
+++ src/portfolio.js
@@ -24,7 +24,7 @@ const greeting = {
   username: "Nikhil Lohiya",
   title: "Hi, I'm Nikhil",
   subTitle: emoji(
-    "A passionate Analytics Engineer 🚀 with 9 years of experience, with expertise in building Generative AI applications and scalable data platforms. Skilled in designing data infrastructure, implementing standards, and leveraging Cloud platforms, Data Lakes, and ETLs to optimize data quality. Proven success in building Gen AI Applications, driving efficiency, reducing costs, and enhancing decision-making through robust analytics solutions and cross-team collaboration. Adept at fostering data utilization and organizational excellence."
+    "Customer-focused and results-driven Product Leader with 9+ years of experience in building and scaling AI-driven and data-intensive products. Passionate about building Data Products, migrations and leveraging Generative AI, and Cloud to solve complex business challenges while driving innovation. Proven expertise in developing product vision, strategy, and roadmaps while leading cross-functional teams to deliver impactful AI-powered solutions. Adept at collaborating with executives, engineering, and go-to-market teams to maximize customer value and business growth."
   ),
   // "Innovative and results-oriented Business with 9 years of professional experience and 2 years of specialized experience in building and scaling Data & Generative AI applications. Expertise in driving the product strategy, development, and deployment of cutting-edge AI-driven solutions that solve complex problems and enhance user experiences. Proven ability to lead cross-functional teams, collaborate with engineering and data science teams, to create AI-powered products that deliver measurable business impact. Adept at navigating the rapidly evolving AI landscape, managing product lifecycles, and aligning AI innovation with customer needs and organizational goals."
   // "A passionate Business Intelligence Engineer 🚀 with 8+ years of professional experience in analyzing and developing solutions spanning database development & architecture, management and predictive analytics, forecasting, and developing interactive dashboards to steer data driven problem solving in enterprises. Expertise in handling large data sets, insights generation, output interpretation, BI automation, and advanced visualizations."
@@ -54,19 +54,16 @@ const skillsSection = {
   ),
   skills: [
     emoji(
-      "⚡ Expertise in defining AI product roadmaps, aligning with business objectives, and driving strategic initiatives across cross-functional teams. "
+      "⚡ Expertise in defining AI & Data product roadmaps, aligning with business objectives, and driving strategic initiatives across cross-functional teams. "
     ),
     emoji(
-      "⚡ Experienced in overseeing the end-to-end lifecycle of AI products, from ideation through launch and iteration, ensuring alignment with company goals."
+      "⚡ Experienced in overseeing the end-to-end lifecycle of Data products, from ideation through launch and iteration, ensuring alignment with company goals."
     ),
     emoji(
-      "⚡ Experienced in collaborating with cross-functional teams to implement LLM solutions that align with business goals and improve operational efficiency. "
+      "⚡ Experienced in collaborating with cross-functional teams to implement LLM based solutions that align with business goals and improve operational efficiency. "
     ),
     emoji(
       "⚡  Adept at managing AI product backlogs, prioritizing features, and translating complex technical requirements into clear, actionable plans. "
-    ),
-    emoji(
-      "⚡ In-depth understanding of BI best practices, including data governance, quality assurance, and maintaining scalable data pipelines. "
     )
   ],
 
@@ -177,22 +174,22 @@ const educationInfo = {
       schoolName: "Stevens  Institute of Technology",
       logo: require("./assets/images/stevensLogo.png"),
       subHeader: "Master of Science in Business Intelligence & Analytics",
-      duration: "September 2017 - December 2018",
+      // duration: "September 2017 - December 2018",
       desc: "Highlights",
       descBullets: [
-        "Recepient of Provost Masters Scholarship Award 2017.",
-        "Treasurer of Business Intelligence Club 2017-2018"
+        "Recepient of Provost Masters Scholarship Award.",
+        "Treasurer of Business Intelligence Club."
       ]
     },
     {
       schoolName: "MNIT Jaipur",
       logo: require("./assets/images/mnitLogo.png"),
       subHeader: "Bachelor of Technology in Electrical Engineering",
-      duration: "July 2012 - April 2016",
+      // duration: "July 2012 - April 2016",
       desc: "Highlights",
       descBullets: [
-        "Mentored over 700 Students as 'Chief Mentor' in Student Mentorship Program(2015-16)",
-        "Founder & 1st Chairman of Electrical Engineering Society, MNIT Jaipur(2015-16)"
+        "Mentored over 700 Students as 'Chief Mentor' in Student Mentorship Program.",
+        "Founder & 1st Chairman of Electrical Engineering Society, MNIT Jaipur."
       ]
     }
   ]
@@ -208,16 +205,16 @@ const techStack = {
       progressPercentage: "90%" //Insert relative proficiency in percentage
     },
     {
-      Stack: "ETLs, Analytics and Databases",
+      Stack: "Product Lifecycle Management",
+      progressPercentage: "80%"
+    },
+    {
+      Stack: "ETLs, Analytics and Data Design",
       progressPercentage: "80%"
     },
     {
       Stack: "Gen-AI Application Design",
       progressPercentage: "85%"
-    },
-    {
-      Stack: "Product Lifecycle Management", //Insert stack or technology you have experience in
-      progressPercentage: "80%" //Insert relative proficiency in percentage
     }
   ],
   displayCodersrank: false
@@ -229,22 +226,19 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Sr. Business Intelligence Engineer",
+      role: "Sr. Business Intelligence Engineer / Product Lead",
       company: "AWS",
       companylogo: require("./assets/images/aws_logo.png"),
       date: "March 2021 – Present",
       desc: "Highlights:",
       descBullets: [
-        "Led development of a multi-Agent platform on Native AWS, managing a team of 5 engineers to implement 4 RAG models and tool invocation, resulting in a chatbot serving 4000+ customers with projected monthly savings of 13,000 work hours. (Multi-Agent Orchestration, LLMs, AWS Bedrock)",
-        "Spearheaded design of the reporting schema infrastructure including consolidation of Data Sources on Data Lakes, ETLs migration, Governance and Data Quality on Datawarehouse as a Service. The resulted in an increase in availability of data tables from 80% to 99.99% and data quality issues by 70%.",
-        "Led the development of an insight engine surfacing 20 KPIs on an in-house application platform supporting over 3000 customers daily for operational reporting.",
-        "Established standardized guidelines for an Org of 100 engineers on schema design and asset nomenclature. This resulted in reduced creation of over 90 new assets with a 30% MoM decrease in support tickets.",
-        "Designed & built 9 standardized data assets supporting over 400 reporting resources (Data Tables & Dashboards) with over 400k query annual references, supporting 8 AWS Organizations.",
-        "Architected and implemented a comprehensive Data Certifications Program, elevating ETL design standards and dashboard UI consistency across the organization. This initiative resulted in a 60% reduction in redundant data assets, a 35% improvement in dashboard usability CSAT scores, and a 25% increase in cross-team data utilization.",
-        "Spearheaded the creation and execution of a comprehensive 3-year content management strategy for the chatbot, achieving alignment across leadership on unstructured data handling & storage while reducing stale feedback by 40%.",
-        "Led development of AWS Partner Org P&L, delivering partner-level cost and profitability insights to Finance Leadership, there by facilitating strategic decisions and annual planning. Created a 3-year plan for P&L integration with rest of AWS Organizations and central Finance teams.",
-        "Established a robust LLOps pipeline for continuous model improvement and integration testing resulting in 99.99% availability of the application and an automated knowledge base expansion based on positive user feedback.",
-        "Led successful integration of 2 acquired chatbot products into the existing suite and 100% data migration within 3 months."
+        "•	Owned and scaled an AI-powered RAG-based chatbot, reducing operational workload by 13,000+ hours/month and improving efficiency by 25% while leading a cross functional team.",
+        "Led end-to-end product strategy for a 3-year roadmap on unstructured data management, securing leadership buy-in and reducing stale feedback by 40%, boosting customer satisfaction by 15%.",
+        "Defined and implemented AI observability features, improving feature prioritization and adoption by 20%, enhancing model transparency.",
+        "Drove cross-functional collaboration across engineering, data science, and leadership to ensure seamless AI integration, achieving 99.99% uptime.",
+        "Standardized data governance policies, improving data discoverability and reducing redundant asset creation by 30% MoM.",
+        "Optimized ETL and ML workflows, reducing cloud compute costs by 18% and improving analytics efficiency in the organizations.",
+        "Standardized Data Practices Across Organizations by implementing efficient schema design, improving query performance by 40%."
       ]
     },
     {
@@ -254,11 +248,12 @@ const workExperiences = {
       date: "February 2019 – Present",
       desc: "Highlights:",
       descBullets: [
-        "Gathered detailed requirements, defined KPIs, developed and implemented solutions for over 12 projects in an Agile setup for 3 clients.",
-        "Built and automated over 30 ETL Process’ & 20 Dashboards using Bigquery/Redshift/Oracle/Tableau, Python/Alteryx and reported in Tableau / Power BI supporting 4 multimillion-dollar product launches.",
-        "Created 2 Tableau Dashboards ingesting more than 50 million rows, and optimized performance from 90 seconds to under 5 seconds for 4000 concurrent users.(Tabjolt / Tabmon)",
-        "Created a “Portfolio Analyzer Dashboard” using Tableau-Python API, with dynamic optimization in Python based on user selected parameters on a Tableau UI.",
-        "Created a Python data stream to interact with databases and automating using python schedular to support 2 POCs."
+        "Launched data-driven insights products supporting 4 multibillion-dollar MNCs by automating 50+ ETL processes leveraging BigQuery, Redshift, and Oracle.",
+        "Designed and implemented KPIs for 25+ dashboards supporting Product, Finance, and Operations teams, improving data-driven decision-making.",
+        "Optimized large-scale analytics dashboards ingesting 50M+ rows, reducing query response time from 90s to under 5s for 4,000+ users.",
+        "Led end-to-end automation of financial and operational reports, decreasing report generation time from 6 hours to 30 minutes.",
+        "Collaborated with engineering teams to design scalable data pipelines, reducing ETL failures by 25% and improving overall system reliability.",
+        "Implemented predictive analytics models, enabling real-time business insights, leading to 15% better forecasting accuracy for financial teams."
       ]
     }
   ]
@@ -287,8 +282,11 @@ const workExperiencesPrevious = {
       date: "October 2016 – July 2017",
       desc: "Highlights:",
       descBullets: [
-        "Improved performance in Batch Data Processing Utility by reducing I/O time with Database by 4%.",
-        "Resolved database performance and deadlock issues for 3 clients in the Oracle Revenue Management and Billing batch data processing cycles with 5 million rows (appx)."
+        "Enhanced batch data processing efficiency by reducing I/O time by 4%, improving database query execution.",
+        "Resolved performance bottlenecks in revenue management systems, improving transaction processing for enterprise clients.",
+        "Developed and optimized SQL queries, reducing query execution time by 30% and improving system responsiveness.",
+        "Collaborated with cross-functional teams to streamline database migration, reducing downtime and ensuring smooth transitions to Revenue Management & Billing.",
+        "Designed and implemented indexing strategies, leading to a 20% improvement in overall system performance."
       ]
     },
     {
@@ -325,8 +323,8 @@ const workExperiencesPrevious = {
       desc: "Highlights:",
       descBullets: [
         "Formerly known as i-deal: Artstreak",
-        "Co-founded and led the development of a non-profit startup focused on creating an evangelist-assisted crowdfunding platform for artists, aligning product vision with business objectives.",
-        "Led user research and data-driven insights to iterate on platform features, improving user engagement and establishing baseline on crowdfunding success rates for artists.",
+        "Founded and led a non-profit startup focused on crowdfunding for artists, securing early-stage funding and partnerships.",
+        "Drove product strategy by leveraging user research and data analytics, increasing artist success rates in fundraising.",
         "Managed product lifecycle, from concept to launch, ensuring timely and efficient execution of product features and platform enhancements.",
         "Led product initiatives that fostered a strong sense of community among artists and backers, building long-term loyalty and increasing retention rates."
       ]
@@ -345,14 +343,57 @@ const openSource = {
 // Fin-Finicky Stuff here ----
 
 const bigProjects = {
-  title: emoji("Fin-Finicky: A Financial Analysts' Tookit 🛠️"),
-  subtitle:
-    "Check it out!!:  A one stop shop for Financial Analysts! (Academic Project Built in R / R Shiny)",
+  title: "Some Cool Academic Projects",
+  subtitle: "Check it out!!",
   projects: [
     {
       image: require("./assets/images/Finfincky.PNG"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      projectName: emoji("Fin-Finicky: A Financial Analysts' Tookit 🛠️"),
+      projectDesc:
+        "• Designed and developed a web application for Financial Analysts (Business Application) calculating ACF, ARIMA, VAR, GARCH analysis on any ticker given by the user using Yahoo Finance API in R and R shiny. Created an Optimized Portfolio & Risk Estimation feature for the individual investors on the user defined stocks. Performed detailed analysis on the commodities Crude, Soy and Gold using the Fin Finicky.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://nlohiya.shinyapps.io/Fin-Finicky/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/Finfincky.PNG"),
+      projectName: emoji(
+        "Sentiment Analysis for Stock Market - Blue Chip Stocks. 🛠️"
+      ),
+      projectDesc:
+        "Created a python engine to pipeline the process of downloading, cleaning, vectorizing and classifying tweets via twitter API by benchmarking amongst 8 supervised & 1 unsupervised learning techniques. Using the most efficient technique, performed sentiment analysis on the daily frequency of the tweets to explain the movement of market securities with an accuracy of 61%.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://nlohiya.shinyapps.io/Fin-Finicky/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/Finfincky.PNG"),
+      projectName: emoji("Duck Wisdom - Portfolio Analysis Tool 🛠️"),
+      projectDesc:
+        "Created a web application using R Shiny for portfolio optimization and risk analysis. For any number of stock symbols application fetches historical data for the stocks and provides calculated features such as Risk Profile for each stock (Skewness, Kurtosis & Correlations) and provide annualized return table. Provides optimized weights for the stocks using minimum variance markowitz theory.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://nlohiya.shinyapps.io/Fin-Finicky/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/Finfincky.PNG"),
+      projectName: emoji(
+        "Demand Side Bidding Strategy in Electricity Markets using GA"
+      ),
+      projectDesc:
+        "Determined optimal demand-side bidding strategy for an Electricity aggregator in Forward electricity market using Spanish Electricity Market Data. The proposed strategy was developed by creating a moving window (12 hours of intraday trades and a futures quote of 24 hours prior to the closing time) and using the differences between the quotes and the bids of an existing player. Thereby, created a cost function and optimized the same by running simulation in a genetic algorithm for 100 generations.",
       footerLink: [
         {
           name: "Visit Website",
@@ -403,10 +444,15 @@ const achievementSection = {
     },
 
     {
-      title: "USEReady: February Spot Award Winner",
-      subtitle: "Won spot award - February 2020",
-      image: require("./assets/images/UsereadyLogo.png"),
-      footerLink: [{name: "Certification", url: ""}]
+      title: "Chronos Certified Mentor",
+      subtitle: "Interviewed over 45 candidates & Mentored 10 in AWS.",
+      image: require("./assets/images/Chronos.jpg"),
+      footerLink: [
+        {
+          name: "Certificate",
+          url: "https://certificates.chronus.com/certificates/cf-2a0b5006-40ed-4102-b4ff-66da0e370617-1256432"
+        }
+      ]
     }
   ],
   display: true // Set false to hide this section, defaults to true
